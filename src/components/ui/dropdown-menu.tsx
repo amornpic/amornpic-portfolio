9eec2890471b371051ced5dd4f3f7a96d558c283
amornpic <amornpic09@gmail.com>
2024-10-21 18:45:18 +0700

    adjust dropdown-menu

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -179,22 +179,6 @@ const DropdownMenuShortcut = ({
 };
 DropdownMenuShortcut.displayName = 'DropdownMenuShortcut';
 
-const DropdownMenuComponent = ({ MenuTrigger }) => {
-	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger>{MenuTrigger}</DropdownMenuTrigger>
-			<DropdownMenuContent>
-				<DropdownMenuLabel>My Account</DropdownMenuLabel>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem>Profile</DropdownMenuItem>
-				<DropdownMenuItem>Billing</DropdownMenuItem>
-				<DropdownMenuItem>Team</DropdownMenuItem>
-				<DropdownMenuItem>Subscription</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
-	);
-};
-
 export {
 	DropdownMenu,
 	DropdownMenuTrigger,
@@ -211,5 +195,4 @@ export {
 	DropdownMenuSubContent,
 	DropdownMenuSubTrigger,
 	DropdownMenuRadioGroup,
-	DropdownMenuComponent,
 };
